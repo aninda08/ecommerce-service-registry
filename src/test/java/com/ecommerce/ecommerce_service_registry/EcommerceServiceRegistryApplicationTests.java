3caec2218085ab2e0f4460238bb65544e83f235b
Aninda Chakraborty <aninda.death@gmail.com>
2025-05-05 23:13:12 +0530

    Initial commit

---
+++ src/test/java/com/ecommerce/ecommerce_service_registry/EcommerceServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce_service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
